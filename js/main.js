de7a3081bc09510d638c9bbc2394d7a1548360e9
Brian Tomlinson <darthlukan@gmail.com>
2015-03-21 21:21:09 +0100

    let's get this rewrite rolling

--- js/main.js
+++ js/main.js
@@ -6,43 +6,5 @@
  */
 
 $(function () {
-    function defaultContent() {
-        $.get("../html/home.html", function(data) {
-            $(".container").html(data);
-        });
-    }
-
-    function render(event) {
-		event.preventDefault();
-        var section = event.target.id + ".html";
-
-        $.get("../html/" + section, function(data) {
-            $(".container").hide(1000, "linear", function() {
-                $(".container")
-                    .fadeOut(1000)
-                    .fadeIn(2000)
-                    .html(data);
-            });
-        });
-    }
-
-    defaultContent();
-
-    function maintenance(event) {
-        event.preventDefault();
-		alert("This section is under maintenance, check back later!");
-    }
-
-    $("#home").click(function(event) {
-		render(event);
-	});
-    $("#projects").click(function(event) {
-		maintenance(event);
-	});
-    $("#social").click(function(event) {
-		 maintenance(event);
-	});
-    $("#random").click(function(event){
-		maintenance(event);
-	});
+    // TODO: Stuff
 });
