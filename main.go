2fe6e818d5afbdd8f6e309872feb63ea41d645c2
Brian Tomlinson <darthlukan@gmail.com>
2015-03-27 18:29:21 +0100

    start again, let's try gin, I hear it is pretty fast

--- main.go
+++ main.go
@@ -3,47 +3,16 @@
 package main
 
 import (
-	"github.com/darthlukan/bct/controllers"
-	"github.com/laurent22/ripple"
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	app := ripple.NewApplication()
-	pingController := controllers.NewPingController()
-	app.RegisterController("ping", pingController)
-
-	// TODO: Properly split out the site handler from the API/Resource handler
-	//app.AddRoute(ripple.Route{Pattern: "/"})
-	app.AddRoute(ripple.Route{Pattern: ":_controller"})
-	//http.HandleFunc("/", TemplatedHandler)
-	app.SetBaseUrl("/")
-	http.HandleFunc("/", app.ServeHTTP)
-
-	log.Printf("Server loaded on localhost:3000\n")
-	http.ListenAndServe(":3000", &LoggedHttpHandler{app})
+	app := gin.Default()
+	app.GET("/ping", pinger)
+	app.Run(":3000")
 }
 
-type LoggedHttpHandler struct {
-	baseHandler http.Handler
-}
-
-func (handler *LoggedHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	go log.Printf("%s Request for %s from %s", r.Method, r.RequestURI, r.RemoteAddr)
-	handler.baseHandler.ServeHTTP(w, r)
-}
-
-func TemplatedHandler(w http.ResponseWriter, r *http.Request) {
-	var content []byte
-
-	splitPath := strings.Split(r.URL.Path, "/")
-	htmlName := splitPath[len(splitPath)-1]
-	if htmlName == "" {
-		content = []byte("Put a pretty HTML index here")
-	} else {
-		content = []byte(htmlName) // Load named HTML
-	}
-	w.Write(content)
+func pinger(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"status": "OK!", "data": "PONG!"})
 }
