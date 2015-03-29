00c5ce9bfafabaa38b7f1d2c28566840c9e8cb09
Brian Tomlinson <darthlukan@gmail.com>
2015-03-29 13:30:15 +0200

    two mobile resolutions and orientations basically structured

--- js/main.js
+++ js/main.js
@@ -4,7 +4,38 @@
  * @contact brian.tomlinson@linux.com
  *
  */
+(function () {
+    var React = require('react');
+    var Card = React.createFactory(require('react-user-card'));
 
-$(function () {
-    // TODO: Stuff
+    var cardStl = {
+        backgroundColor: 'rgba(40,40,40,0.8)',
+        width: 'inherit',
+        height: 'inherit',
+        margin: 'inherit',
+        padding: '1%'
+    }
+
+    var headerStl = {
+        backgroundColor: '#901111',
+        padding: '1%',
+        color: '#ECEFF1',
+        position: 'relative'
+    }
+
+    var contentStl = {
+        color: 'inheriit',
+        padding: '2%'
+    }
+
+    React.render(
+            Card({
+                userimg: '/img/me.jpg',
+                username: 'DarthLukan',
+                content: '"We\'re all humans, act accordingly."',
+                cardStyle: cardStl,
+                headerStyle: headerStl,
+                contentStyle: contentStl
+            }), document.getElementById('card')
+    );
 });
