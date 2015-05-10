ddbb8214cef53f39529a7557ac6eb5d34b39db8f
Brian Tomlinson <darthlukan@gmail.com>
2015-05-10 21:21:15 +0200

    split out handlers from main

--- main.go
+++ main.go
@@ -3,9 +3,7 @@
 package main
 
 import (
-	"github.com/darthlukan/bct/quotes"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
@@ -27,22 +25,4 @@ func main() {
 
 	// Engage
 	app.Run(":3000")
-}
-
-func index(context *gin.Context) {
-	index := "index.html"
-	context.HTML(http.StatusOK, index, gin.H{})
-}
-
-func pinger(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"status": "OK!", "data": "PONG!"})
-}
-
-func quoter(context *gin.Context) {
-	quote := quotes.RandomQuote()
-	if len(quote) > 0 {
-		context.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": quote})
-	} else {
-		context.JSON(http.StatusInternalServerError, gin.H{"status": "Failed", "data": "Error, quote not found."})
-	}
 }
